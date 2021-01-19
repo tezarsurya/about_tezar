63578a235fa7441edd4d045c1a44ba990a183b47
tezarsurya <59132336+tezarsurya@users.noreply.github.com>
2021-01-19 13:51:31 +0700

    move images to public/images

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,9 @@ import React from "react";
 const Skills = () => {
   return (
     <section id="skills">
-      <div className="skills__left"></div>
+      <div className="skills__left">
+        <h3>Programming Skills</h3>
+      </div>
       <div className="skills__right"></div>
     </section>
   );
